f0f2bc80e21f89379953fd018a838c186655986f
Martin <marb61a@gmail.com>
2020-08-27 12:28:04 +0100

    Added single and multipl user basic test

---
+++ tests/01-script.js
@@ -0,0 +1,17 @@
+// Calling Google
+// What happens when 1000's of users call
+import http from 'k6/http';
+
+// The entry point function
+export default function(){
+    // Make a http request to google, ensure https is used
+    http.get('http://www.google.com');
+}
+
+// To execute with 1 virtual user
+// k6 run tests/01-script.js
+// test successful
+
+// To execute with 10 virtual users
+// This is similar to multiple parallel while loops
+// k6 run --vus 10 --duration 5s tests/01-script.js
