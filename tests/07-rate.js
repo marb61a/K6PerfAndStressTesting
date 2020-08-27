386b51af786e51cd3eb933f46dde6c7cc3e0c915
Martin <marb61a@gmail.com>
2020-08-27 20:30:11 +0100

    Added rate test

---
+++ tests/07-rate.js
@@ -0,0 +1,24 @@
+// Because checks will not cause an entire test suite to fail
+// There is a need to use Rate alongside checks
+
+import http from 'k6/http';
+import { check } from 'k6';
+
+// Rate allows settings fail rate conditions eg Test suite fails
+// if 10% fail then the suite fails, if less than 10% fail then the
+// test suite will pass
+import { Rate } from 'k6/metrics';
+
+// String errors is not important it can be anything but will be seen in logs
+export let errorRate = new Rate('errors');
+
+export default function(){
+    let response = http.get('https://run.mocky.io/v3/cec79dd8-cc39-40de-934e-e3630fc5e841');
+
+    console.log(`Resonse body length ${response.body.length} for VU = ${__VU} ITERATION ${__ITER}`);
+
+    check(response, {
+        "Response status is 200 " : (r) => r.status === 200,
+        "Body size is 43 bytes " : (r) => r.body.length == 43
+    });
+}
