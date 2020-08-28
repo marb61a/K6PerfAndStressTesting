bf97eaeaa2ff4f5d80f7df4d6698dd05a099a3ba
Martin <marb61a@gmail.com>
2020-08-28 19:41:26 +0100

    Updated rate test

--- tests/07-rate.js
+++ tests/07-rate.js
@@ -12,13 +12,30 @@ import { Rate } from 'k6/metrics';
 // String errors is not important it can be anything but will be seen in logs
 export let errorRate = new Rate('errors');
 
+// Define options
+export let options = {
+    thresholds : {
+        // 10% error
+        errors: ["rate<0.1"]
+    }
+}
+
 export default function(){
     let response = http.get('https://run.mocky.io/v3/cec79dd8-cc39-40de-934e-e3630fc5e841');
 
     console.log(`Resonse body length ${response.body.length} for VU = ${__VU} ITERATION ${__ITER}`);
 
-    check(response, {
-        "Response status is 200 " : (r) => r.status === 200,
+    const check1 = check(response, {
+        "Response status is 200 " : (r) => r.status === 200
+    });
+
+    // A boolean which needs to be set to negative
+    errorRate.add(!check1);
+
+    const check2 = check(response, {
         "Body size is 43 bytes " : (r) => r.body.length == 43
     });
+
+    // A boolean which needs to be set to negative
+    errorRate.add(!check2);    
 }
