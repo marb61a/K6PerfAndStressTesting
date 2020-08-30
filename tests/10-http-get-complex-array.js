f6f4e4cadbac20dc34e439306a5d87c67193c3f5
Martin <marb61a@gmail.com>
2020-08-30 20:15:47 +0100

    Updated parse array test and addded initial complex array test

---
+++ tests/10-http-get-complex-array.js
@@ -0,0 +1,47 @@
+[
+    {
+        "name": "Joe Bloggs",
+        "email": "joe@example.com",
+        "job": "Web Developer",
+        "location": "London",
+        "array": [
+            1, 2, 3, 4, 5
+        ]
+    },
+    {
+        "name": "Jack Black",
+        "email": "jack@example.com",
+        "job": "Software Tester",
+        "location": "Dublin",
+        "array": [
+            2, 3, 4, 5, 1
+        ]
+    },
+    {
+        "name": "Dave Smith",
+        "email": "dave@example.com",
+        "job": "Manager",
+        "location": "New York",
+        "array": [
+            3, 4, 5, 1, 2
+        ]
+    },
+    {
+        "name": "John Jones",
+        "email": "john@example.com",
+        "job": "Engineer",
+        "location": "Manchester",
+        "array": [
+            4, 5, 1, 2, 3
+        ]
+    },
+    {
+        "name": "Gordon Bennett",
+        "email": "gordon@example.com",
+        "job": "Trader",
+        "location": "Glasgow",
+        "array": [
+            5, 1, 2, 3, 4
+        ]
+    }
+]
