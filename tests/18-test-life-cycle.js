73a0a24092084181072a49a67e3db85063f0ae4c
Martin <marb61a@gmail.com>
2020-09-02 12:26:34 +0100

    Added fixed-rps and updated test-lifecycle test

--- tests/18-test-life-cycle.js
+++ tests/18-test-life-cycle.js
@@ -5,21 +5,24 @@ var counter = 1;
 
 // 2 - Setup 
 // This will be called once before load test starts
-// It is called one time
+// It is called one time, there will be no iteration in logs
 export function setup(){
     console.log(`Inside setup ${counter}`);
+    // Will be the data passed to main
+    return "Today is Wednesday";
 }
 
 // 3 - Default
 // The main function which is the default entry point for virtual users
-export default function(){
-    console.log(`Inside setup ${counter} VU=${__VU} ITER=${__ITER}`);
+// Data can be anything - Will return undefined
+export default function(data){
+    console.log(`Inside default ${counter} VU=${__VU} ITER=${__ITER} DATA is ${data}`);
     counter = counter + 1;
 }
 
 // Teardown
 // This function gets called once default function has finished
-// It is called one time
-export function teardown(){
-    console.log(`Inside setup ${counter}`);
+// It is called one time, there will be no iteration in logs
+export function teardown(data){
+    console.log(`Inside teardown ${counter} DATA is ${data}`);
 }
